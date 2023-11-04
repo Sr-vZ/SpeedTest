f77064d62d35331e2f1ac29aceba8ba0e130912c
Sr-vZ <srvz39@gmail.com>
2023-11-05 02:46:45 +0530

    STYLE: UI minor modifications in css

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,4 @@
+const HTTP_TIMEOUT = 10000
 const start_test = document.getElementById("start_test")
 const test_stats_card = document.getElementById("test_stats_card")
 test_stats_card.style.display = "none"
@@ -13,36 +14,52 @@ async function generateFile(sizeInBytes) {
     return new Blob([buffer]);
 }
 
+async function logChunks(url, { signal }) {
+    const response = await fetch(url, { signal });
+    for await (const chunk of response.body) {
+        // Do something with the chunk
+        console.log(chunk)
+    }
+}
+
 async function testSpeed() {
     test_stats_card.style.display = "block"
+    document.getElementById("test_spinner").classList.remove("hidden")
     const downloadUrl = "/download";
-    const uploadFile = await generateFile(25 * 1024 * 1024); // 25 MB
+    const uploadFile = await generateFile(5 * 1024 * 1024); // 25 MB
 
     // Measure download speed
     const downloadStart = performance.now();
     // await fetch(`/download-speed?url=${downloadUrl}`);
-    await fetch(downloadUrl, {
+    res = await fetch(downloadUrl, {
         headers: {
             'Cache-Control': 'no-cache'
         }
     });
+    const aborter = new AbortController();
+    logChunks("/download", { signal: aborter.signal });
     const downloadEnd = performance.now();
-    const downloadSpeed = 25 / ((downloadEnd - downloadStart) / 1000); // Convert to Mbps
+    downloadedSize = parseInt(res.headers.get('Content-Length')) / 1024 / 1024
+    const downloadSpeed = downloadedSize / ((downloadEnd - downloadStart) / 1000); // Convert to Mbps
     document.getElementById("download-speed").textContent = downloadSpeed.toFixed(2);
 
     // Measure upload speed
     const formData = new FormData();
     formData.append("file", uploadFile);
 
+    const controller = new AbortController();
+    const timeoutId = setTimeout(() => controller.abort(), HTTP_TIMEOUT);
     const uploadStart = performance.now();
     await fetch("/upload", {
         method: "POST",
         body: formData,
+        // signal: controller.signal
     });
     const uploadEnd = performance.now();
-    const uploadSpeed = 25 / ((uploadEnd - uploadStart) / 1000); // Convert to Mbps
+    const uploadSpeed = 5 / ((uploadEnd - uploadStart) / 1000); // Convert to Mbps
 
 
     document.getElementById("upload-speed").textContent = uploadSpeed.toFixed(2);
+    document.getElementById("test_spinner").classList.add("hidden")
     start_test.disabled = false
 }
